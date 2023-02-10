83fa7016774deb0121cc29de80c8226b764e1b09
joshunrau <contact@joshuaunrau.com>
2023-02-10 10:44:42 -0500

    setup internationalization

--- src/services/i18next.ts
+++ src/services/i18next.ts
@@ -1,28 +1,16 @@
 import i18next from 'i18next';
 import LanguageDetector from 'i18next-browser-languagedetector';
+import Backend from 'i18next-http-backend';
 import { initReactI18next } from 'react-i18next';
 
 void i18next
+  .use(Backend)
   .use(LanguageDetector)
   .use(initReactI18next)
   .init({
     fallbackLng: 'en',
     interpolation: {
       escapeValue: false
-    },
-    resources: {
-      en: {
-        translation: {
-          greeting: "Hi, I'm Josh",
-          slogan: 'I build websites.'
-        }
-      },
-      fr: {
-        translation: {
-          greeting: "Bonjour, je m'appelle Josh",
-          slogan: 'Je crée des sites web'
-        }
-      }
     },
     returnObjects: true,
     supportedLngs: ['en', 'fr']
