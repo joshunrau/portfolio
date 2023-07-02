eab50ddc5bbdef11569043a09c66744e55f8ba8e
joshunrau <contact@joshuaunrau.com>
2023-07-02 16:15:15 -0400

    update i18n

--- src/i18n/index.ts
+++ src/i18n/index.ts
@@ -1,29 +1,26 @@
 import en from "./locales/en.json";
 import fr from "./locales/fr.json";
 
-export const languages = {
-  en: "English",
-  fr: "Français"
-};
+export const languages = ["en", "fr"] as const;
 
-export type Language = keyof typeof languages;
-
-export const defaultLang = "en" satisfies Language;
+export type Language = (typeof languages)[number];
 
 export const translations: Record<Language, Partial<typeof en>> = { en, fr };
 
+export type TranslationKey = keyof (typeof translations)[Language];
+
 export function extractLanguage(url: URL) {
   const locale = url.pathname.split("/")[1];
-  if (locale in translations) {
-    return locale as keyof typeof translations;
+  if (locale === "fr") {
+    return locale;
   }
-  return defaultLang;
+  return "en";
 }
 
 export function useTranslations(url: URL) {
   const resolvedLanguage = extractLanguage(url);
-  const t = (key: keyof (typeof translations)[typeof defaultLang]) => {
-    return (translations[resolvedLanguage][key] || translations[defaultLang][key])!;
+  const t = (key: TranslationKey) => {
+    return translations[resolvedLanguage][key] || translations["en"][key]!;
   };
   return { resolvedLanguage, t };
 }
