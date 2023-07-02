b633538ac78a8c38eed1585279832d620d568256
joshunrau <contact@joshuaunrau.com>
2023-07-02 11:04:59 -0400

    add some fonts

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,3 +1,5 @@
+const defaultTheme = require("tailwindcss/defaultTheme");
+
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
@@ -7,6 +9,10 @@ module.exports = {
       container: {
         center: true
       }
+    },
+    fontFamily: {
+      display: ["Lato", ...defaultTheme.fontFamily.sans],
+      sans: ["Montserrat", ...defaultTheme.fontFamily.sans]
     }
   },
   plugins: []
