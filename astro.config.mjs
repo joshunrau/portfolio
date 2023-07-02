38b66ddc9a66cd260dc848a089a423793b32f101
joshunrau <contact@joshuaunrau.com>
2023-07-02 15:46:52 -0400

    add seo

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,15 +1,31 @@
 import { defineConfig, sharpImageService } from "astro/config";
-
 import tailwind from "@astrojs/tailwind";
+
+import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
-  compressHTML: true,
+  //compressHTML: true,
   experimental: {
     assets: true
   },
-  integrations: [tailwind()],
+  integrations: [
+    tailwind(),
+    sitemap({
+      i18n: {
+        defaultLocale: "en",
+        locales: {
+          en: "en",
+          fr: "fr"
+        }
+      }
+    })
+  ],
   image: {
     service: sharpImageService()
-  }
+  },
+  site: "https://joshuaunrau.com"
+  // redirects: {
+  //   "/": "/en"
+  // }
 });
