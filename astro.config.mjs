82f0aa0be3b0168d671ff669913f6055d2ee7f40
joshunrau <contact@joshuaunrau.com>
2023-07-02 13:01:08 -0400

    add section

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,15 @@
-import { defineConfig } from "astro/config";
+import { defineConfig, sharpImageService } from "astro/config";
 
 import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
   compressHTML: true,
-  integrations: [tailwind()],
   experimental: {
     assets: true
+  },
+  integrations: [tailwind()],
+  image: {
+    service: sharpImageService()
   }
 });
