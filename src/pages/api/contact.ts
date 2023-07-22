f1c2ad62a7f0594441739c0eb5487dbd5c51e1e2
joshunrau <contact@joshuaunrau.com>
2023-07-22 16:10:58 -0400

    add contact form

---
+++ src/pages/api/contact.ts
@@ -0,0 +1,39 @@
+import type { APIRoute } from "astro";
+import { createTransport } from "nodemailer";
+
+export const prerender = false;
+
+export const post: APIRoute = async ({ request }) => {
+  const formData = await request.formData();
+  const { name, email, subject, message } = Object.fromEntries(formData.entries()) as Record<string, string>;
+
+  const transport = createTransport({
+    host: process.env.SMTP_HOST,
+    port: parseInt(process.env.SMTP_PORT ?? "587"),
+    secure: false,
+    auth: {
+      user: process.env.SMTP_USER,
+      pass: process.env.SMTP_PASS
+    }
+  });
+
+  const mail = {
+    from: { name, address: process.env.SMTP_USER! },
+    to: process.env.CONTACT_EMAIL,
+    subject,
+    text: `New message from ${name} <${email}>\n\n${message}`
+  };
+
+  transport.sendMail(mail, (err, info) => {
+    if (err) {
+      throw err;
+    }
+    console.log(info);
+  });
+
+  return {
+    body: JSON.stringify({
+      message: "This was a POST!"
+    })
+  };
+};
