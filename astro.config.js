40034279719041e3d252001fe78348a2540f5cad
joshunrau <contact@joshuaunrau.com>
2024-06-29 13:39:11 -0400

    initial commit

---
+++ astro.config.js
@@ -0,0 +1,27 @@
+import sitemap from '@astrojs/sitemap';
+import tailwind from '@astrojs/tailwind';
+import { defineConfig, sharpImageService } from 'astro/config';
+
+export default defineConfig({
+  build: {
+    assets: '_assets'
+  },
+  compressHTML: true,
+  image: {
+    service: sharpImageService()
+  },
+  integrations: [
+    sitemap({
+      i18n: {
+        defaultLocale: 'en',
+        locales: {
+          en: 'en',
+          fr: 'fr'
+        }
+      }
+    }),
+    tailwind()
+  ],
+  output: 'static',
+  site: 'https://joshuaunrau.com'
+});
---
+++ eslint.config.js
@@ -0,0 +1,6 @@
+import { config } from '@joshunrau/eslint-config';
+
+export default config({
+  astro: { enabled: true },
+  react: { enabled: false }
+});
