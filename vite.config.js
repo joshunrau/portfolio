1a0d714293b74f5e2d29e666118bce44537592c7
joshunrau <contact@joshuaunrau.com>
2024-07-01 17:08:00 -0400

    setup wasm

--- vite.config.js
+++ vite.config.js
@@ -2,14 +2,18 @@ import { sveltekit } from '@sveltejs/kit/vite';
 import autoprefixer from 'autoprefixer';
 import tailwindcss from 'tailwindcss';
 import { defineConfig } from 'vite';
+import wasm from 'vite-plugin-wasm';
 
 export default defineConfig({
+  build: {
+    target: 'es2022'
+  },
   css: {
     postcss: {
       plugins: [autoprefixer(), tailwindcss()]
     }
   },
-  plugins: [sveltekit()],
+  plugins: [sveltekit(), wasm()],
   server: {
     port: 4000
   }
